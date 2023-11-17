bb324f402470a18d593772244e680b1e4d365b42
Masrafi Anam <masrafianam@gmail.com>
2023-11-17 16:48:51 +0600

    Show anam

---
+++ lib/show_anam.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowAnam extends StatefulWidget {
+  const ShowAnam({super.key});
+
+  @override
+  State<ShowAnam> createState() => _ShowAnamState();
+}
+
+class _ShowAnamState extends State<ShowAnam> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
