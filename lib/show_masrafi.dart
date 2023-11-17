6fd99544a2e04a17d2cbfeccfda3c0c3632187f0
Masrafi Anam <masrafianam@gmail.com>
2023-11-17 16:37:33 +0600

    Masrafi push

---
+++ lib/show_masrafi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowMasrafi extends StatefulWidget {
+  const ShowMasrafi({super.key});
+
+  @override
+  State<ShowMasrafi> createState() => _ShowMasrafiState();
+}
+
+class _ShowMasrafiState extends State<ShowMasrafi> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
